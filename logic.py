60d827de0ba2ec4bd4ff7d329a387ef85f508c3c
Diego <diiegoglzz@gmail.com>
2026-07-07 12:44:46 +0200

    feat: books_per_year function

--- logic.py
+++ logic.py
@@ -11,3 +11,7 @@ def top_authors(df, n=10):
 def most_reviewed_books(df, n=10):
     resultado = df.sort_values('Num_Customers_Rated', ascending=False).drop_duplicates(subset=['Book_Title'])
     return resultado[['Book_Title', 'Author','Num_Customers_Rated', 'Rating']].head(n)
+
+def books_per_year(df):
+    resultado = df.groupby('Year')['Book_Title'].count()
+    return resultado
--- main.py
+++ main.py
@@ -6,4 +6,4 @@ df = file_io.load_csv("data/bestsellers.csv")
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
-print(logic.most_reviewed_books(df))
+print(logic.books_per_year(df))
