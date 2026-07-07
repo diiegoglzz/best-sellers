7bc3444ad9518ccfbbec4154e273f421d3aa3127
Diego <diiegoglzz@gmail.com>
2026-07-07 12:38:39 +0200

    feat: most_reviewed function

--- logic.py
+++ logic.py
@@ -7,3 +7,7 @@ def top_books_by_rating(df, n=10):
 def top_authors(df, n=10):
     resultados = df.groupby('Author')['Book_Title'].count()
     return resultados.sort_values(ascending=False).head(n)
+
+def most_reviewed_books(df, n=10):
+    resultado = df.sort_values('Num_Customers_Rated', ascending=False).drop_duplicates(subset=['Book_Title'])
+    return resultado[['Book_Title', 'Author','Num_Customers_Rated', 'Rating']].head(n)
